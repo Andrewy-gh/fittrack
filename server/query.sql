e54ba5fa7db79f97297f6093855ce9d64d47c140
Andy-git985 <andy.yu617@gmail.com>
2025-06-07 11:53:24 -0400

    add sql files required for sqlc generate

---
+++ server/query.sql
@@ -0,0 +1,62 @@
+-- Basic SELECT queries
+-- name: GetWorkout :one
+SELECT * FROM workout WHERE id = $1;
+
+-- name: ListWorkouts :many  
+SELECT * FROM workout ORDER BY date DESC;
+
+-- name: GetExercise :one
+SELECT * FROM exercise WHERE id = $1;
+
+-- name: ListExercises :many
+SELECT * FROM exercise ORDER BY name;
+
+-- name: GetSet :one
+SELECT * FROM "set" WHERE id = $1;
+
+-- name: ListSets :many
+SELECT * FROM "set" ORDER BY id;
+
+-- INSERT queries for form submission
+-- name: CreateWorkout :one
+INSERT INTO workout (date, name, notes)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: GetOrCreateExercise :one
+INSERT INTO exercise (name) 
+VALUES ($1)
+ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name
+RETURNING *;
+
+-- name: CreateSet :one
+INSERT INTO "set" (exercise_id, workout_id, weight, reps, set_type)
+VALUES ($1, $2, $3, $4, $5)
+RETURNING *;
+
+-- Batch insert for better performance (optional)
+-- name: CreateSets :copyfrom
+INSERT INTO "set" (exercise_id, workout_id, weight, reps, set_type)
+VALUES ($1, $2, $3, $4, $5);
+
+-- Complex queries for joining data
+-- name: GetWorkoutWithSets :many
+SELECT 
+    w.id as workout_id,
+    w.date as workout_date,
+    w.name as workout_name,
+    w.notes as workout_notes,
+    s.id as set_id,
+    s.weight,
+    s.reps,
+    s.set_type,
+    e.id as exercise_id,
+    e.name as exercise_name
+FROM workout w
+JOIN "set" s ON w.id = s.workout_id
+JOIN exercise e ON s.exercise_id = e.id
+WHERE w.id = $1
+ORDER BY e.name, s.id;
+
+-- name: GetExerciseByName :one
+SELECT * FROM exercise WHERE name = $1;
