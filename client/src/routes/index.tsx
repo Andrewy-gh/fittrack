63e67681f285857536286686394283b9932b8148
Andy-git985 <andy.yu617@gmail.com>
2025-06-26 21:00:45 -0400

    exercise select fetch from backend

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,28 +1,20 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { WorkoutEntryForm } from '@/components/workout-entry-form';
+import type { ExerciseOption } from '@/lib/types';
 
 export const Route = createFileRoute('/')({
-  // loader: async () => {
-  //   const res = await fetch('/api/hello');
-  //   if (!res.ok) {
-  //     throw new Error('Failed to fetch data');
-  //   }
-  //   const data = await res.json();
-  //   return data;
-  // },
-  loader: () => {
-    const exercises = [
-      { id: 1, name: 'Squat' },
-      { id: 2, name: 'Deadlift' },
-      { id: 3, name: 'Bench Press' },
-    ];
-    return exercises;
+  loader: async () => {
+    const res = await fetch('/api/exercises');
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await res.json();
+    return data as ExerciseOption[];
   },
   component: App,
 });
 
 function App() {
-  // const { message } = Route.useLoaderData();
   const exercises = Route.useLoaderData();
   return (
     <main className="container mx-auto space-y-4 p-4 md:p-12">
