14cb68e7e2f0a0fbd3d3287b279118368c71e27e
Andy-git985 <andy.yu617@gmail.com>
2025-06-27 20:48:11 -0400

    update workout styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from '@tanstack/react-router'
 
 export default function Header() {
   return (
-    <header className="p-2 flex gap-2 bg-white text-black justify-between">
+    <header className="flex justify-between gap-2 border-b border-neutral-700 bg-neutral-800 p-2 text-white">
       <nav className="flex flex-row">
         <div className="px-2 font-bold">
           <Link to="/">Home</Link>
@@ -12,5 +12,5 @@ export default function Header() {
         </div>
       </nav>
     </header>
-  )
+  );
 }
