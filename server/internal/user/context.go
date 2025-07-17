46267d07016e409b6e0c8a4d737fbe14f2fc9ca0
Andy-git985 <andy.yu617@gmail.com>
2025-07-17 00:00:28 -0400

    use db.User. remove defined struct

--- server/internal/user/context.go
+++ server/internal/user/context.go
@@ -2,18 +2,20 @@ package user
 
 import (
 	"context"
+
+	db "github.com/Andrewy-gh/fittrack/server/internal/database"
 )
 
 type ctxKey struct{}
 
 // WithContext adds a user to the given context.
-func WithContext(ctx context.Context, u User) context.Context {
+func WithContext(ctx context.Context, u db.Users) context.Context {
 	return context.WithValue(ctx, ctxKey{}, u)
 }
 
 // Current retrieves the user from the context.
 // It returns the user and true if the user was found, otherwise it returns an empty user and false.
-func Current(ctx context.Context) (User, bool) {
-	u, ok := ctx.Value(ctxKey{}).(User)
+func Current(ctx context.Context) (db.Users, bool) {
+	u, ok := ctx.Value(ctxKey{}).(db.Users)
 	return u, ok
 }
