0055eaa5a9a6390a68a795f99dce655ba64fdf61
Andy-git985 <andy.yu617@gmail.com>
2025-07-30 23:41:23 -0400

    combobox test

--- client/src/components/ui/command.tsx
+++ client/src/components/ui/command.tsx
@@ -70,9 +70,10 @@ function CommandInput({
       <SearchIcon className="size-4 shrink-0 opacity-50" />
       <CommandPrimitive.Input
         data-slot="command-input"
-        // removed text-sm
         className={cn(
           "placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
+          "text-base md:text-sm",
+          "min-h-[44px]",
           className
         )}
         {...props}
@@ -103,7 +104,6 @@ function CommandEmpty({
   return (
     <CommandPrimitive.Empty
       data-slot="command-empty"
-      // removed text-sm
       className="py-6 text-center"
       {...props}
     />
@@ -118,7 +118,6 @@ function CommandGroup({
     <CommandPrimitive.Group
       data-slot="command-group"
       className={cn(
-        // removed [&_[cmdk-group-heading]]:text-xs
         'text-foreground [&_[cmdk-group-heading]]:text-muted-foreground overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5  [&_[cmdk-group-heading]]:font-medium',
         className
       )}
@@ -148,7 +147,6 @@ function CommandItem({
     <CommandPrimitive.Item
       data-slot="command-item"
       className={cn(
-        // removed text-sm
         "data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
@@ -165,7 +163,6 @@ function CommandShortcut({
     <span
       data-slot="command-shortcut"
       className={cn(
-        // removed text-xs
         "text-muted-foreground ml-auto tracking-widest",
         className
       )}
