fa5f079606469be7a54216c6fa65fdb1f2a99310
Andy-git985 <andy.yu617@gmail.com>
2025-08-27 22:16:22 -0400

    remove comment

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -2,10 +2,8 @@ import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { QueryClient } from '@tanstack/react-query';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import Header from '../components/Header';
-// import type { User } from '@/lib/api/auth';
 
 interface RouteContext {
-  // user: User;
   queryClient: QueryClient;
 }
 
