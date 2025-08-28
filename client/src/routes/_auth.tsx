4de789fb4941551d01c27bfe58d0f5b1470b2640
Andy-git985 <andy.yu617@gmail.com>
2025-08-27 22:16:39 -0400

    remove comments

--- client/src/routes/_auth.tsx
+++ client/src/routes/_auth.tsx
@@ -4,17 +4,6 @@ import { stackClientApp } from '@/stack';
 export const Route = createFileRoute('/_auth')({
   beforeLoad: async () => {
     const user = await stackClientApp.getUser({ or: 'redirect' });
-    console.log('beforeLoad running')
-    // if (!user) {
-    //   throw new Error('User not found');
-    // }
-    // if (!user.id || typeof user.id !== 'string') {
-    //   throw new Error('User ID not found');
-    // }
-    // const { accessToken } = await user.getAuthJson();
-    // if (!accessToken) {
-    //   throw new Error('Access token not found');
-    // }
     return { user };
   },
   component: LayoutComponent,
