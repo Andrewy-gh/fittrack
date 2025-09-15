9ab27b38995a3a2416c7499b2b6b4f4d22cbf1da
Andy-git985 <andy.yu617@gmail.com>
2025-09-14 20:01:37 -0400

    delete header

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { QueryClient } from '@tanstack/react-query';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-import Header from '../components/Header';
 
 interface RouteContext {
   queryClient: QueryClient;
@@ -10,7 +9,6 @@ interface RouteContext {
 export const Route = createRootRouteWithContext<RouteContext>()({
   component: () => (
     <>
-      <Header />
       <Outlet />
       <TanStackRouterDevtools />
     </>
