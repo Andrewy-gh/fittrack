c362cee259c3a93f4385ab6c04d0354c5f0a061b
Andy-git985 <andy.yu617@gmail.com>
2025-11-24 17:46:46 -0500

    feat: add autofocus to exercise search inputs

--- client/src/routes/_layout/workouts/-components/add-exercise-screen.tsx
+++ client/src/routes/_layout/workouts/-components/add-exercise-screen.tsx
@@ -84,6 +84,7 @@ export const AddExerciseScreen = withForm({
           <div className="relative">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground w-4 h-4" />
             <Input
+              autoFocus
               placeholder="Search/add exercises"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
